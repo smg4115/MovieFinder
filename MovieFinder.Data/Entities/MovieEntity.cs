561ddb4eba280b3d0a557c5225442178cb507e42
Mitch Boling <mphboling@gmail.com>
2024-06-27 20:50:02 -0400

    Added migrations

--- MovieFinder.Data/Entities/MovieEntity.cs
+++ MovieFinder.Data/Entities/MovieEntity.cs
@@ -5,8 +5,14 @@ namespace MovieFinder.Data.Entities;
 
 public class MovieEntity
 {
+    [Key]
+    public int MovieId { get; set; }
+    [Required]
     public string MovieTitle { get; set; } = null;
+    [Required]
     public string MovieDirector { get; set; } = null;
+    [Required]
     public string MpaRating { get; set; } = null;
+    [Required]
     public int MovieGenre { get; set; }
 }
--- MovieFinder.Data/Migrations/20240627233453_MovieEntity.Designer.cs
+++ MovieFinder.Data/Migrations/20240627233453_MovieEntity.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MovieFinder.Data;
 
@@ -11,9 +12,11 @@ using MovieFinder.Data;
 namespace MovieFinder.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240627233453_MovieEntity")]
+    partial class MovieEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
