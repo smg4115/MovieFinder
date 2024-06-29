a0ed44a7455305d448f5dd38a0521fafc2df4dee
Solomon Haynes <solomonhaynes22@gmail.com>
2024-06-29 15:22:53 -0400

    made a small change to one of the models.  cs1504 error when building

--- MovieFinder.Models/Review/ReviewDetail.cs
+++ MovieFinder.Models/Review/ReviewDetail.cs
@@ -7,9 +7,4 @@ namespace MovieFinder.Models.Review;
 
 public class ReviewDetail
 {
-  public int Id { get; set; }
-  public int MovieId { get; set; }
-  public int UserId { get; set; }
-  public double Rating { get; set; }
-  public string Comment { get; set; } = string.Empty;
-}
+  
--- MovieFinder.Models/Review/ReviewListItem.cs
+++ MovieFinder.Models/Review/ReviewListItem.cs
@@ -3,16 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace MovieFinder.Models.Review
+namespace MovieFinder.Models.Review;
+public class ReviewListItem
 {
-    public class ReviewListItem
-    {
 
-        public int Id { get; set; }
-
-        public int MovieId { get; set; }
-
-        public int UserId { get; set; }
-
-    }
-}
+    public int Id { get;
